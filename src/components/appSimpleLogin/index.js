25c31c515dc86818f9c534a7d9c9e389f801c1c5
Rodrigo Rocha <rodrigo.desenvolvedor.web@gmail.com>
2020-12-26 11:54:30 -0300

    Password and email validation

--- src/components/appSimpleLogin/index.js
+++ src/components/appSimpleLogin/index.js
@@ -11,6 +11,14 @@ const appSimpleLogin = () => {
         password:''
     }
 
+    const props = {
+        validation: {
+            email: null,
+            password: null,
+            enableButton: false
+        },
+    }
+
     const hooks = ({ methods }) => ({
         beforeOnInit () {
             authStore.on(methods.isValidAuth)
@@ -20,11 +28,19 @@ const appSimpleLogin = () => {
     const events = ({ on, queryOnce, methods }) => ({
         onTypeEmail () {
             const emailInput = queryOnce('#email')
-            on('keyup', emailInput, methods.setEmail, true, 500)
+            on('keyup', emailInput, ({target}) => {
+                methods.setEmail(target)
+                methods.toggleButton()
+                methods.setFocus(target, queryOnce)
+            }, true, 100)
         },
         onTypePassword () {
             const passwordInput = queryOnce('#password')
-            on('keyup', passwordInput, methods.setPassword, true, 500)
+            on('keyup', passwordInput, ({target}) => {
+                methods.setPassword(target)
+                methods.toggleButton()
+                methods.setFocus(target, queryOnce)
+            }, true, 100)
         },
         onClickButton () {
             const button = queryOnce('#btnLogin')
@@ -32,26 +48,93 @@ const appSimpleLogin = () => {
         }
     })
 
-    const methods = ({ setState, getState }) => ({
-        setEmail ({ target }) {
+    const methods = ({ setState, getState, getProps, setProps }) => {
+
+        const _setCursorPosition = (element) => {
+            const textLength = element.value.length
+
+            if (typeof element.selectionStart == "number") {
+                element.focusStart = textLength
+                element.focusEnd = textLength
+            }   
+            
+            element.setSelectionRange(textLength, textLength + 1)
+            element.focus()
+        }
+
+        const _emailValidate = (target, key) => {
+            if(!key || key !== 'email') return
+
+            const props = getProps()
+            const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
+            const isValid = emailPattern.test(target.value)
+            const validation = props.validation
+            
+            if(isValid) {
+                validation.email = 'validInput'
+                const payload = { ...props, ...{ validation }}
+                return setProps(payload)
+            }
+
+            validation.email = 'invalidInput'
+            const payload = { ...props, ...{ validation }}
+            return setProps(payload)            
+
+        }
+        const _passwordValidate = (target, key) => {
+            if(!key || key !== 'password') return
+
+            const props = getProps()
+            const passwordPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/
+            const isValid = passwordPattern.test(target.value)
+            const validation = props.validation
+
+            if(isValid) { 
+                validation.password = 'validInput'
+                const payload = { ...props, ...{ validation }}
+                return setProps(payload)
+            }
+
+            validation.password = 'invalidInput'
+            const payload = { ...props, ...{ validation }}
+            return setProps(payload)    
+        }
+
+        const _setValidationClass = (target, key) => {
+            _emailValidate(target, key)
+            _passwordValidate(target, key)
+        }
+
+        const setFocus = (target, queryOnce) => {
+            const selector = `#${target.getAttribute('id')}`
+            const element = queryOnce(selector)
+            _setCursorPosition(element)
+        }
+
+        const setEmail = (target) => { 
             const { value: email } = target
             const state = getState()
             const payload = Object.assign(state, { email })
             setState(payload)
-        },
-        setPassword ({ target }) {
+            _setValidationClass(target, 'email')
+        }
+
+        const setPassword = (target) => {
             const { value: password } = target
             const state = getState()
             const payload = Object.assign(state, { password })
             setState(payload)
-        },
-        authenticate () {
+            _setValidationClass(target, 'password')
+        }
+
+        const authenticate = () => {
             const state = getState()
             const token = authService.authenticate(state)
             if(token) authStore.update({ token })
 
-        },
-        isValidAuth () {
+        }
+
+        const isValidAuth = () => {
             const token = authStore.get('token')
             const isValidToken = authService.validateToken(token)
             if(isValidToken) {
@@ -60,10 +143,39 @@ const appSimpleLogin = () => {
                 setTimeout(() => authStore.update({ token: ''}), 10000)
             }
         }
-    })
+
+        const toggleButton = () => {
+            const props = getProps()
+
+            const { email, password } = props.validation
+            const validation = props.validation
+            const isValid = email === 'validInput' && password === 'validInput' 
+
+            if(isValid) { 
+                validation.enableButton = true
+                const payload = { ...props, ...{ validation }}
+                return setProps(payload)
+            }
+
+            validation.enableButton = false
+            const payload = { ...props, ...{ validation }}
+            return setProps(payload)    
+
+        }
+
+        return {
+            setEmail,
+            setPassword,
+            authenticate,
+            isValidAuth,
+            setFocus,
+            toggleButton
+        }
+    }
 
     return {
         state,
+        props,
         template,
         styles,
         hooks,
--- src/components/appSimpleLogin/template.js
+++ src/components/appSimpleLogin/template.js
@@ -1,17 +1,34 @@
-export default ({ state, html }) => html`
+export default ({ props, state, html }) => {
+
+    return html`
     <div class="simple__login__wrapper">
         <div class="form form--vertical">
             <label  class="form__label">
                 <span class="form__span"> E-mail </span>
-                <input type="text" class="form__input" id="email" value="${state.email}">
+                <input 
+                    type="text" 
+                    class="form__input ${props.validation.email}" 
+                    id="email" 
+                    value="${state.email}"
+                >
             </label>
             <label  class="form__label">
                 <span class="form__span"> Senha </span>
-                <input type="text" class="form__input" id="password" value="${state.password}">
+                <input 
+                    type="text" 
+                    class="form__input ${props.validation.password}" 
+                    id="password" 
+                    value="${state.password}"
+                >
             </label>
-            <button class="btn btn__login btn--hover" id="btnLogin"> 
+            <button 
+                class="btn btn__login btn--hover  ${props.validation.enableButton ? '': 'disabled'}" 
+                id="btnLogin" 
+                ${props.validation.enableButton ? '': 'disabled'}
+            > 
                 <i class="las la-sign-in-alt btn__icon"></i> Entrar 
             </button>
         </div>
     </div>
 `
+}
