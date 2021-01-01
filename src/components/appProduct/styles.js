cb934a011979b1b0217046833a2a7bd79b1acc59
Rodrigo Rocha <rodrigo.desenvolvedor.web@gmail.com>
2021-01-01 13:36:16 -0300

    conclusion appProduct

--- src/components/appProduct/styles.js
+++ src/components/appProduct/styles.js
@@ -36,6 +36,7 @@ export default ({ ctx, css }) => css`
     ${ctx} .product__title,
     ${ctx} .product__description,
     ${ctx} .product__price,
+    ${ctx} .product__call_action,
     ${ctx} .product__amount {
         display:flex;
         width:100%;
@@ -48,7 +49,7 @@ export default ({ ctx, css }) => css`
     ${ctx} .product__title {
         font-size: 1.2em;
         font-weight: 500;
-        color:#487def
+        color:#666
     }
 
     ${ctx} .price__label {
@@ -66,18 +67,55 @@ export default ({ ctx, css }) => css`
         text-align:right;
         border-top-right-radius:3px;
         border-bottom-right-radius:3px;       
-        color:#487def;
+        color:#999;
     }
 
     ${ctx} .product__description {
         line-height: 1.5em
-    }    
+    }   
+    
+    ${ctx} .product__amount {
+        align-items:center;
+        justify-content: space-between;
+        border:1px #ebebeb solid;
+        border-radius:3px;
+        margin-top:15px;
+        padding:0
+    }
+    
+    ${ctx} .amount__value {
+        padding:5px;
+        font-weight:500;
+        width:45px;
+        text-align:center;
+    }
 
     ${ctx} .btn {
         font-size: 1em;
         padding:10px 15px;
         margin-top:0;
         border-radius:3px;
+    }
+
+    ${ctx} .amount__btn_icon {
+        display:flex;
+        align-items:center;
+        justify-content:center;
+        width:45px;
+        height:45px;
+        padding:5px;
+        margin:2px;
+        color:#999;
+        background:#f1f1f1;
+        border-radius:3px;
+        transition: .3s ease-in-out;
+        cursor: pointer;
+        font-size: 1.5em
+    }
+
+    ${ctx} .amount__btn--hover:hover {
+        color:#fff;
+        background:#ef4859
     }
 
 `
--- src/components/appProduct/template.js
+++ src/components/appProduct/template.js
@@ -12,6 +12,11 @@ export default ({ state, html }) => html`
             <span class="price__label">R$ 27,90</span>
         </div>
         <div class="product__amount">
+            <i class="las la-minus-circle amount__btn_icon amount__btn--hover"></i>
+            <span class="amount__value">07</span>
+            <i class="las la-plus-circle amount__btn_icon amount__btn--hover"></i>
+        </div>
+        <div class="product__call_action">
             <a href="#/" class="btn btn--danger">
                 <i class="las la-cart-arrow-down btn__icon"></i>  Adicionar ao carrinho
             </a>
